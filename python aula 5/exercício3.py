3ffbd837cd1d1187321cf30cdc46322f0d73a39b
heitor-dn <heitor.dnvs@gmail.com>
2026-03-09 22:31:56 -0300

    Add files via upload

---
+++ python aula 5/exercício3.py
@@ -0,0 +1,6 @@
+tabuada=int(input("Digite o número do qual deseja saber a tabuada: "))
+x=0
+while x<10:
+    x+=1
+    resultado=x*tabuada
+    print(f"{tabuada}x{x}={resultado}")
---
+++ python aula 5/exercício4.py
@@ -0,0 +1,13 @@
+x=0
+for i in range(0,6):
+    y = int(input("Digite um número inteiro e positivo: "))
+    if y > x:
+        x = y
+print(f"O maior é o {x}")
+    
+    
+    
+
+    
+
+
