70df6348faadd7cd51f75c2508f543483aa51a01
heitor-dn <heitor.dnvs@gmail.com>
2026-04-09 15:49:34 -0300

    Delete python aula 5 directory

--- python aula 5/exercício1.py
+++
@@ -1,6 +0,0 @@
-impar=int(input("Digite o ultimo digito da contagem: "))
-x=0
-while x<=impar:
-    if x % 2 != 0:
-        print(x)
-    x=x+1
--- python aula 5/exercício7.py
+++
@@ -1,7 +0,0 @@
-x=0
-y=0
-while x<10:
-    i=int(input("Digite um número: "))
-    y=y+i
-    x+=1
-print("A somatoria dos números é ", y)
