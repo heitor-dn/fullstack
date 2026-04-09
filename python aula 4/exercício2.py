530509bee068aa5440767f0ec960d1474e04e1fa
heitor-dn <heitor.dnvs@gmail.com>
2026-04-09 15:49:27 -0300

    Delete python aula 4 directory

--- python aula 4/exercício2.py
+++
@@ -1,8 +0,0 @@
-valor1=int(input("Digite um número inteiro: "))
-valor2=int(input("Digite outro número inteiro: "))
-valor3=int(input("Digite um último número inteiro: "))
-resultado= sorted([valor1, valor2, valor3], reverse=True)
-if (valor1==valor2) or (valor1==valor3) or (valor2==valor3):
-    print("Os números inseridos devem ser diferentes.")
-else:
-    print("Os números organizados em ordem decrescente são:", resultado)
